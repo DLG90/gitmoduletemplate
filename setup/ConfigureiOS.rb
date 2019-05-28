9d76a3b673666fe9a1fcd7c362ea2a46daba4c25
杜联冈 <1574259130@qq.com>
2019-05-28 12:03:39 +0800

    无操作

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -12,7 +12,6 @@ module Pod
     end
 
     def perform
-
 #      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
        keep_demo = "no"
 #      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
@@ -21,21 +20,16 @@ module Pod
         when :specta
           configurator.add_pod_to_podfile "Specta"
           configurator.add_pod_to_podfile "Expecta"
-
           configurator.add_line_to_pch "@import Specta;"
           configurator.add_line_to_pch "@import Expecta;"
-
           configurator.set_test_framework("specta", "m", "ios")
-
         when :kiwi
           configurator.add_pod_to_podfile "Kiwi"
           configurator.add_line_to_pch "@import Kiwi;"
           configurator.set_test_framework("kiwi", "m", "ios")
-
         when :none
           configurator.set_test_framework("xctest", "m", "ios")
       end
-
 #      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
       snapshots = "no"
       case snapshots
